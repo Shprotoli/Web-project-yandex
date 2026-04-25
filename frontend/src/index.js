15622936f11d9f7b13380fc9db6828d4669276d9
= <ggrerqq2346@rambler.ru>
2026-04-25 21:58:02 +0300

    add [frontend]: Add header

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from "./reportWebVitals";
 
+import Header from "./Components/header";
+
+import "./styles/main.scss"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <p>123</p>
+    <Header />
   </React.StrictMode>
 );
 
