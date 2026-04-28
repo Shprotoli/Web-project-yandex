f50641a4a1a3866e750bd1227a64addc75b6b6e2
= <ggrerqq2346@rambler.ru>
2026-04-28 19:04:43 +0300

    add [frontend]: Add blitz list

--- frontend/src/Pages/Categorys.js
+++ frontend/src/Pages/Categorys.js
@@ -2,9 +2,45 @@ import Header from "../Components/header";
 
 import "../styles/page/categorys/banner.scss"
 import "../styles/page/categorys/subobjects.scss"
+import "../styles/page/categorys/blitzs.scss"
 import "../styles/page/categorys/categorys-main.scss"
 
 import rusTestImg from '../assets/img/category/hero/rus_test.png';
+
+function Element() {
+    return (
+        <li className={"blitzs__list-element"}>
+            <article className={"blitz-card"}>
+                <ul className={"tag__list"}>
+                    <li className={"tag__list-element"}>9 заданий</li>
+                    <li className={"tag__list-element"}>#NBLITZ1</li>
+                </ul>
+                <p className={"title"}>Синонимы</p>
+                <p className={"description"}>Проверь свои знания слов и их значений: подбери правильные синонимы и расширь свой словарный запас!</p>
+                <ul className={"button__list"}>
+                    <button className={"button__list-element"}>
+                        Решать
+                        <svg style={{
+                            marginLeft: "36px",
+                        }} width="30" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="26" height="26" rx="5" fill="white"/>
+                            <path d="M8.05124 16.7199C7.71278 17.0583 7.71278 17.6071 8.05124 17.9455C8.38969 18.284 8.93843 18.284 9.27689 17.9455L8.66406 17.3327L8.05124 16.7199ZM18.1974 8.66602C18.1974 8.18737 17.8094 7.79935 17.3307 7.79935H9.53073C9.05208 7.79935 8.66406 8.18737 8.66406 8.66602C8.66406 9.14466 9.05208 9.53268 9.53073 9.53268H16.4641V16.466C16.4641 16.9447 16.8521 17.3327 17.3307 17.3327C17.8094 17.3327 18.1974 16.9447 18.1974 16.466V8.66602ZM8.66406 17.3327L9.27689 17.9455L17.9436 9.27884L17.3307 8.66602L16.7179 8.05319L8.05124 16.7199L8.66406 17.3327Z" fill="#7E14FF"/>
+                        </svg>
+                    </button>
+                    <button className={"button__list-element"}>
+                        Поделиться
+                        <svg style={{
+                            marginLeft: "24px",
+                        }} width="34" height="30" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="26" height="26" rx="5" fill="#7E14FF"/>
+                            <path d="M10.75 8.16667L13 6M13 6L15.25 8.16667M13 6V13.2222M9.25017 11.0556C8.55126 11.0556 8.20181 11.0556 7.92614 11.1655C7.55861 11.3121 7.26642 11.5935 7.11418 11.9474C7 12.2128 7 12.5492 7 13.2222V16.6889C7 17.4978 7 17.9021 7.16349 18.211C7.3073 18.4828 7.5366 18.7042 7.81885 18.8427C8.1394 19 8.55924 19 9.39768 19H16.6027C17.4411 19 17.8604 19 18.1809 18.8427C18.4631 18.7042 18.6929 18.4828 18.8366 18.211C19 17.9024 19 17.4985 19 16.6911V13.2222C19 12.5492 18.9999 12.2128 18.8858 11.9474C18.7335 11.5935 18.4415 11.3121 18.074 11.1655C17.7983 11.0556 17.4489 11.0556 16.75 11.0556" stroke="white" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
+                </ul>
+            </article>
+        </li>
+    )
+}
 
 function BannerElement() {
     return (
@@ -75,6 +111,22 @@ function SubjectsElement() {
     )
 }
 
+function BlitzsElement() {
+    return (
+        <>
+            <aside className={"blitzs"}>
+                <ul className={"blitzs__list"}>
+                    <Element />
+                    <Element />
+                    <Element />
+                    <Element />
+                    <Element />
+                </ul>
+            </aside>
+        </>
+    )
+}
+
 function CategorysPage() {
     return (
         <>
@@ -82,6 +134,7 @@ function CategorysPage() {
             <main>
                 <BannerElement />
                 <SubjectsElement />
+                <BlitzsElement />
             </main>
         </>
     )
